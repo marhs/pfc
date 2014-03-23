52618bca2f674cc60733553abb80638e4f997bdf
Marco Herrero <me@marhs.de>
2014-03-23 17:39:27 +0100

    El KGC recibe usuarios y los almacena. Pide randoms y genera clave

--- base.py
+++ base.py
@@ -11,24 +11,46 @@ class KeyGenerationCenter:
     def __init__(self, keySize):
         self.keySize = keySize
         self.key = self.generateKey(keySize)
-        self.users = dict()
+        self.users = []
+        self.randoms = []
+        self.subkeys = []
+        self.messages = []
+        self.auth = []
+        self.k = 0
         self.active = 0
 
-    def sendUsers(self):
-        print(list(self.users.keys()))
+        self.objUsuarios = []
 
+    def recibeUsuarios(self, listaUsuarios):
+        # Inicio de protocolo, limpia los parametros activos
+        self.users, self.randoms, self.objUsuarios = [],[],[]
+
+        self.objUsuarios = listaUsuarios
+        for usuario in listaUsuarios:
+            self.users.append(usuario.name)
+        return self.users
+
+    def recibeRandoms(self):
+        self.randoms = []
+        for user in self.objUsuarios:
+            self.randoms.append(user.generateRandom())
+        return self.randoms
+            
     # Genera S
     def generateKey(self, keySize):
 
         return getrandbits(self.keySize)
 
     # Divide S en 2 partes por cada usuario. 
-    def generateSubKeys(self, users):
-        
-        for client in users:
-            self.users[client] = self.generateSubKey()
+    def generateSubKeys(self):
+        # Primero limpia las subclaves que pudiesen existir
+        self.subkeys = []
+        for client in self.users:
+            key = self.generateSubKey()
+            self.subkeys.append(key)
 
         return self.users
+
     # Divide S en 2 partes y lo devuelve (usado en la anterior)
     def generateSubKey(self):
 
@@ -38,13 +60,37 @@ class KeyGenerationCenter:
 
         return subkey
 
+    def sendMessage(self):
+        '''
+        for user in range(len(self.users)):
+            g = 13^(self.subkeys[user]+self.random[user]) # TODO Averiguar como se hace la operacion g^x
+            # Hash
+            hs = [self.users[user],0,0,0]
+            self.message.append([g,self.users[user],hs])
+        '''
+        return self.message
+
+    def generateAuth(self):
+        '''
+        a = self.generateSubKs()
+        b = self.users
+        c = self.randoms
+        return [self.k,a,b,c]
+        '''
+        return 0
+
 class User:
 
     def __init__(self, name):
         self.name = name
+        self.random = -1 
+        self.subkey = -1
         self.key = -1
 
     # Una vez se tiene el mensaje que envia el KGC, se recupera la clave.
+    def generateRandom(self):
+        self.random = getrandbits(64)
+        return self.random
     def recoverKey(self, msg):
         #TODO
         
@@ -54,15 +100,19 @@ class User:
 
 kgc = KeyGenerationCenter(8)
 
-
 users = []
 for n in range(8):
-    users.append(User("Name"+str(n)).name)
+    users.append(User("Name"+str(n)))
 
-kgc.generateSubKeys(users)
+kgc.recibeUsuarios(users)
+kgc.recibeRandoms()
+kgc.generateSubKeys()
 print(kgc.key)
 print(kgc.users)
-kgc.sendUsers()
+print(kgc.randoms)
+
+for n in users:
+    print(n.random)
 
 
 
