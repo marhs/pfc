492a6151a60e7408a8d559e3d2abb74a3ca6714b
Marco Herrero <me@marhs.de>
2014-05-04 17:47:37 +0200

    Terminado despejaClave (usando aritmetica modular) FALTA TESTEO

--- utils.py
+++ utils.py
@@ -25,7 +25,19 @@ def mostrarMensaje(mensaje):
         print(msg)
 
 def extEuclides(a,b):
+    x,y, u,v = 0,1, 1,0
+    while a != 0:
+        q, r = b//a, b%a
+        m, n = x-u*q, y-v*q
+        b,a, x,y, u,v = a,r, u,v, m,n
+    gcd = b
+    return [gcd, x, y]
 
-    # TODO
+def modInverse(a,b):
+    eu = extEuclides(a,b)
+    return eu[1]%b
 
-    return [1,2,3,4]
+# Equivalente a resolver la ecuacion clave que devuelve K'
+def despejarClave(gs1,gs2r,gr,mod):
+    r = gs1*gs2r%mod
+    return r * modInverse(gr,mod)%mod
